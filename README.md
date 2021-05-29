7243e1d38d5bcf02d6ca650f1b7a932486584b9f
Tiago Oliveira <olvrti@gmail.com>
2021-05-29 12:49:28 -0300

    chore: add flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_clean_arch
+
+A new Flutter application with clean architecture.
---
+++ android/app/src/main/kotlin/com/github/olvrti/flutter_clean_arch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.olvrti.flutter_clean_arch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
