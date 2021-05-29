cd49294565af5566e25aca9b4afd641a92d14aeb
Tiago Oliveira <olvrti@gmail.com>
2021-05-29 18:14:21 -0300

    test: ensure HttpAdapter calls post request with correct data

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,41 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    @required String url,
+    @required String method,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    client.post(url, headers: headers);
+  }
+}
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  group('post', () {
+    test('Should call post with correct data', () async {
+      final client = ClientSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
+    });
+  });
+}
