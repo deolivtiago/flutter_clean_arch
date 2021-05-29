09a61633c7e4bfe9fc9f272cd68d259fdc1f15e9
Tiago Oliveira <olvrti@gmail.com>
2021-05-29 15:13:43 -0300

    refactor: move api details from domain to data layer

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -14,6 +14,28 @@ class RemoteAuthentication {
   });
 
   Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    await httpClient.request(
+      url: url,
+      method: 'post',
+      body: RemoteAuthenticationParams.fromDomain(params).toJson(),
+    );
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    @required this.email,
+    @required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+        email: params.email,
+        password: params.password,
+      );
+
+  Map toJson() => {'email': email, 'password': password};
 }
