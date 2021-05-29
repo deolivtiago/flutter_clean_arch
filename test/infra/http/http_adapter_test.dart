be5a90b6286027fc8d79e842ccda4dd400e68d8c
Tiago Oliveira <olvrti@gmail.com>
2021-05-29 18:50:23 -0300

    test: ensure HttpAdapter calls post request without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,7 +20,13 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    client.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    client.post(
+      url,
+      headers: headers,
+      body: jsonBody,
+    );
   }
 }
 
@@ -52,6 +58,18 @@ void main() {
           'accept': 'application/json'
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post request without body', () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
