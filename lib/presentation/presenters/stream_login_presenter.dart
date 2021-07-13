f62e6946ee40bae2ba2f6e896f1837fa96749b05
Tiago Oliveira <olvrti@gmail.com>
2021-07-13 19:31:32 -0300

    feat: ensure StreamLoginPresenter emits correct events on InvalidCredentialsError

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:meta/meta.dart';
 
+import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 
 import '../protocols/protocols.dart';
@@ -9,6 +10,7 @@ import '../protocols/protocols.dart';
 class LoginState {
   String email;
   String password;
+  String mainError;
   String emailError;
   String passwordError;
   bool isLoading = false;
@@ -35,14 +37,19 @@ class StreamLoginPresenter {
 
   void validateEmail(String email) {
     _state.email = email;
-    _state.emailError = validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(
+      field: 'email',
+      value: email,
+    );
     _update();
   }
 
   void validatePassword(String password) {
     _state.password = password;
-    _state.passwordError =
-        validation.validate(field: 'password', value: password);
+    _state.passwordError = validation.validate(
+      field: 'password',
+      value: password,
+    );
     _update();
   }
 
@@ -50,21 +57,27 @@ class StreamLoginPresenter {
     _state.isLoading = true;
     _update();
 
-    await authentication.auth(AuthenticationParams(
-      email: _state.email,
-      password: _state.password,
-    ));
+    try {
+      await authentication.auth(AuthenticationParams(
+        email: _state.email,
+        password: _state.password,
+      ));
+    } on DomainError catch (error) {
+      _state.mainError = error.description;
+    }
 
     _state.isLoading = false;
     _update();
   }
 
+  Stream<String> get mainErrorStream =>
+      _controller.stream.map((state) => state.mainError).distinct();
   Stream<String> get emailErrorStream =>
       _controller.stream.map((state) => state.emailError).distinct();
   Stream<String> get passwordErrorStream =>
       _controller.stream.map((state) => state.passwordError).distinct();
-  Stream<bool> get isFormValidStream =>
-      _controller.stream.map((state) => state.isFormValid).distinct();
   Stream<bool> get isLoadingStream =>
       _controller.stream.map((state) => state.isLoading).distinct();
+  Stream<bool> get isFormValidStream =>
+      _controller.stream.map((state) => state.isFormValid).distinct();
 }
