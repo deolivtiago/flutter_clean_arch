94774f83daff29983dc189a4bf17068a112ecba7
Tiago Oliveira <olvrti@gmail.com>
2021-07-13 21:54:06 -0300

    test: ensure EmailValidator returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -7,7 +7,12 @@ class EmailValidation implements FieldValidation {
 
   EmailValidation(this.field);
 
-  String validate(String value) => null;
+  String validate(String value) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo Inválido.';
+  }
 }
 
 void main() {
@@ -27,5 +32,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('valid_email@mail.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('invalid_email'), 'Campo Inválido.');
   });
 }
